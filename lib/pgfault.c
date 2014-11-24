f4545bcbf78e6503bbb7852df904c2d6c4a64306
Jeff Weiner <jdweiner@cs.washington.edu>
2014-11-24 00:30:38 -0800

    More whitespace fixup
    Myles, I'm sorry I'm so OCD about this... :'(

--- lib/pgfault.c
+++ lib/pgfault.c
@@ -29,14 +29,14 @@ set_pgfault_handler(void (*handler)(struct UTrapframe *utf))
 	if (_pgfault_handler == 0) {
 		// First time through!
 		// LAB 4: Your code here.
-    int success =
-      sys_page_alloc(0, (void *)UXSTACKTOP - PGSIZE, PTE_W | PTE_U | PTE_P);
-    if (success != 0) {
-      panic("sys_page_alloc: %d", success);
-    }
+		int success =
+			sys_page_alloc(0, (void *)UXSTACKTOP - PGSIZE, PTE_W | PTE_U | PTE_P);
+		if (success != 0) {
+			panic("sys_page_alloc: %d", success);
+		}
 
-    sys_env_set_pgfault_upcall(0, _pgfault_upcall);
-  }
+		sys_env_set_pgfault_upcall(0, _pgfault_upcall);
+	}
 
 	// Save handler pointer for assembly to call.
 	_pgfault_handler = handler;
